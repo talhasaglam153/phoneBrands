0d779cbfe9fdaee2dbaba01726683a6e096d3032
Mustafa Talha SAGLAM <talhasaglam153@gmail.com>
2022-07-29 14:08:49 +0300

    Service connection

--- app/src/main/java/com/tcoding/phonebrands/network/RetroInstance.kt
+++ app/src/main/java/com/tcoding/phonebrands/network/RetroInstance.kt
@@ -1,4 +1,20 @@
 package com.tcoding.phonebrands.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetroInstance{
+
+    companion object {
+        val BASE_URL = "https://api-mobilespecs.azharimm.site/v2/"
+
+        fun getRetroInstance(): Retrofit{
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        }
+    }
 }
--- app/src/main/java/com/tcoding/phonebrands/network/RetroService.kt
+++ app/src/main/java/com/tcoding/phonebrands/network/RetroService.kt
@@ -1,4 +1,14 @@
 package com.tcoding.phonebrands.network
 
+import com.tcoding.phonebrands.model.Phone
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface RetroService {
+
+
+    @GET("brands")
+    fun getPhoneBrands(): Call<Phone>
+    
+
 }
